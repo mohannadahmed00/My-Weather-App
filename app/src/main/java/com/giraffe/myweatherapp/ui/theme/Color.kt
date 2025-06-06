11d4877f3ab0d751a3c872369b2b423d5b049beb
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-06 09:04:00 +0300

    Add Google Fonts support and new color definitions and include vector assets

--- app/src/main/java/com/giraffe/myweatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/giraffe/myweatherapp/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val lightBlue = Color(0xFF87CEFA)
+val darkBlue = Color(0xFF060414)
+val transBlue = Color(0xFF060414).copy(alpha = .12f)
--- app/src/main/java/com/giraffe/myweatherapp/ui/theme/Type.kt
+++ app/src/main/java/com/giraffe/myweatherapp/ui/theme/Type.kt
@@ -4,9 +4,18 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.giraffe.myweatherapp.R
 
 // Set of Material typography styles to start with
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+val fontFamily = FontFamily(Font(googleFont = GoogleFont("Inter"), fontProvider = provider))
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
