0d7f97695a8b04a3ec24a7d2536638d2e4ae87c2
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-10 14:39:20 +0300

    Remove unused dynamic color scheme code from Theme.kt

--- app/src/main/java/com/giraffe/myweatherapp/ui/theme/Theme.kt
+++ app/src/main/java/com/giraffe/myweatherapp/ui/theme/Theme.kt
@@ -47,11 +47,10 @@ fun MyWeatherAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        /*dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
+        }*/
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
